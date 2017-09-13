c199de6f69291fbd0a9c165793a61f80dfaa7bd6
Jui-Fa Tsai <alpha78178@gmail.com>
2017-09-13 21:15:51 +0800

    Update and add regression models

--- modeling/model.py
+++ modeling/model.py
@@ -2,13 +2,15 @@
 import os, sys, re, time
 import numpy as np
 
-from sklearn.model_selection import cross_val_score, train_test_split, ShuffleSplit
+#from sklearn.cross_validation import cross_val_score 
+from sklearn.model_selection import cross_val_score
 
 class model:
 
     def __init__( self, model, name='' ):
         self.model = model
         self.name  = name
+        self.prediction = None
         self.scores = { 'cv':None, 'model':None, 'resml':None }
         self.is_fitted = False
 
@@ -20,12 +22,12 @@ class model:
 
 
     def predict( self, X ):
-        return self.model.predict( X )
+        self.prediction = self.model.predict( X )
+        return self.prediction
 
 
-    def get_scores( self, X_train, y_train, X_test, y_test, k=4  ):
-        cv = ShuffleSplit( n_splits=k, test_size=0.3, random_state=0 )
-        self.scores['cv']    = cross_val_score( model, X, Y, cv=cv )
+    def get_scores( self, X_train, y_train, X_test, y_test, cv=4  ):
+        self.scores['cv']    = cross_val_score( estimator=model, X=X_train, y=y_train, cv=cv )
         self.scores['model'] = model.score( X_test, y_test ))
    
         sum=0.0
@@ -34,6 +36,7 @@ class model:
             p = np.log(prediction[i]+1)
             r = np.log(y_test[i]+1)
             sum = sum + (p - r)**2
-        self.scores['rmsle'] = (sum/len(prediction))**0.5 
+        self.scores['rmsle'] = (sum/len(prediction))**0.5
+        return self
 
 
--- modeling/regression.py
+++ modeling/regression.py
@@ -3,12 +3,57 @@ import os, sys, re, time
 import numpy as np
 import pandas as pd
 
+from model import *
+
 from sklearn.model_selection import cross_val_score, train_test_split, ShuffleSplit
 from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score, mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
 
 class regression:
 
     def __init__( self ):
-        return
+        self.model={'RF'    : model(RandomForestRegressor(),         'Random Forest'           )],
+                    'LR_p1' : model(linear_model.LinearRegression(), 'Linear Regression'       )],
+                    'LR_p2' : model(linear_model.LinearRegression(), 'Linear Regression 2-Poly')],
+                    'LR_pn' : model(linear_model.LinearRegression(), 'Linear Regression n-Poly')]}
+
+
+    def regFitAll( self, X, 
+                         y,
+                         do_RF   =True,
+                         do_LR_p1=True,
+                         do_LR_p2=True,
+                         do_LR_pn=False,
+                         pn=None):
+
+        if do_RF:    regFitRF( X, y, self.model['RF'])
+        if do_LR_p1: regFitLR( X, y, self.model['LR_p1'], 1 )
+        if do_LR_p2: regFitLR( X, y, self.model['LR_p2'], 2 )
+
+        if do_LR_pn and pn > 2: 
+            regFitLR( X, y, self.model['LR_pn'], pn)
+            self.model['LR_pn'].name = 'Linear Regression %d-Poly'%( int(pn) )
+
+        return self
+
+
+    def regFitRF( self, X, y, estimator=None ):
+        return estimator
+
+    def regFitLR( self, X, y, estimator=None, n_poly=1 ):
+        X_train = X
+        y_train = y
+        if int(n_poly) > 1:
+            quadratic = PolynomialFeatures(degree=int(n_poly))
+            X_train = quadratic.fit_transform(X)
+
+        if not estimator:
+            estimator = model(linear_model.LinearRegression())
+
+        estimator.fit( X_train, y_train )
+        return estimator 
+
+
 
